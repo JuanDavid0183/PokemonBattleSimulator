5f858c8b38070039810c2294d3ab8e78f1f7a6fc
JuanDavid0183 <99076664@mydavinci.nl>
2025-02-14 10:06:06 +0100

    charmander+ program.cs

---
+++ Charmender.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace PokemonBattleSimulator
+//{
+//    internal class Charmender
+//    {
+//    }
+//}
+
+
+//public class Charmender
+using System;
+
+namespace HelloWorld
+{
+    public class Charmander
+    {
+        public string NickName { get; set; }
+        public string Strength = "fire";
+        public string Weakness = "water";
+
+        public Charmander(string NickName, string strength, string weakness)
+        {
+            this.NickName = NickName;
+            this.Strength = strength;
+            this.Weakness = weakness;
+        }
+
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace PokemonBattleSimulator
+﻿namespace PokemonBattleSimulator;
+using System;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+using global::HelloWorld;
+
+
+
+
+    class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Enter charmender name:");
+        string NickName = Console.ReadLine();
+        Charmander charmander = new Charmander(NickName, "fire", "water");
+        Console.WriteLine("the Pokemon his name is " + charmander.NickName + " his Strength is " + charmander.Strength + " and his weaknes is " + charmander.Weakness + "!");
+        for (int i = 0; i < 10; i++)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(charmander.NickName);
         }
     }
 }
+
